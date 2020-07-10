c422442ebbac4c5bd85296a4969b9a2d030108b7
rishvanth-kp <rishvanth@gmail.com>
2020-07-10 12:02:12 -0700

    (1) completed genome2reads.py (2) added mappedLen.py to calulate mappled length distribution (3) minor changes to snakefiles

--- bin_bounds/genome2reads.py
+++ bin_bounds/genome2reads.py
@@ -18,6 +18,7 @@ import sys, argparse
 
 def readGenome(genomeFile):
     genome = []
+    genomeLen = 0
     for line in open(genomeFile):
         line = line.strip()
         if (line[0] == '>'):
@@ -25,25 +26,48 @@ def readGenome(genomeFile):
             genome.append([chrName, ''])
         else:
             genome[-1][1] += line
-    return genome
+            genomeLen += len(line)
+    return genome, genomeLen
+
 
 def main():
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-genome', dest='genomeFile',
                         required=True, help='genome in fasta format')
     parser.add_argument('-l', '--read-len', dest='readLen',
                         type=int, required=True, help='read length')
     parser.add_argument('-n', '--reads-in-file', dest='readsInFile',
-                        type=int, default=1000000, 
-                        help='number of reads per file (default: 100M)')
+                        type=int, default=25000000, required=False,
+                        help='number of reads per file (default: 25M)')
     parser.add_argument('-o', '--outfile-prefix', dest='outfilePrefix',
                         required=True, help='output file name prefix')
+    parser.add_argument('-v', '--verbose', action='store_true', dest='VERBOSE',
+                        required=False, help='print progress to stderr')
     args = parser.parse_args()
-    
 
-    genome = readGenome(args.genomeFile)
-    print(genome)
+    if (args.VERBOSE):
+        print('[READING GENOME]', file = sys.stderr)
+    genome, genomeLen = readGenome(args.genomeFile)
+    if (args.VERBOSE):
+        print('\tChromosomes: %d' % (len(genome)), file=sys.stderr)
+        print('\tGenome len: %d' % (genomeLen), file=sys.stderr)
+
+    if (args.VERBOSE):
+        print('[WRITING READS]', file = sys.stderr)
+    fileCount = 0
+    for chrs in genome:
+        if (args.VERBOSE):
+            print('\t%s: %d' % (chrs[0], len(chrs[1])))
+        for i in range(0, len(chrs[1]) - args.readLen + 1):
+            if (i % args.readsInFile == 0):
+                fileName = '%s_%d.fa' % (args.outfilePrefix, fileCount)
+                out = open(fileName, 'w')
+                fileCount += 1
+            readName = '%s:%d' % (chrs[0], i)
+            readSeq = chrs[1][i:i+args.readLen]
+            print('%s\n%s' % (readName, readSeq), file = out)
+
 
 if __name__ == '__main__':
     main()
---
+++ bin_bounds/mappedLen.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020 Rishvanth Prabakar
+#
+# Authors: Rish Prabakar
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import sys, argparse
+import pysam
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-sam', dest='samFile',
+                        required=True, help='mapped sam file')
+    parser.add_argument('-q', '--min-mapq', dest='minMapq', type=int, 
+                        default=0, required=False, help='min MAPQ to consider')
+    parser.add_argument('-o', '--out-file', dest='outFile',
+                        required=True, help='out file')
+    args = parser.parse_args()
+
+    alignmentLen = {}
+    totalMapped = 0  
+    mappedReads = pysam.AlignmentFile(args.samFile, 'r')
+    for read in mappedReads:
+        if read.is_read1 and read.mapping_quality >= args.minMapq:
+            totalMapped += 1
+            alnLen = read.query_alignment_length
+            if alnLen in alignmentLen:
+                alignmentLen[alnLen] += 1
+            else:
+                alignmentLen[alnLen] = 1
+    mappedReads.close()
+
+    out = open(args.outFile, 'w')
+    for length, count in sorted(alignmentLen.items()):
+        print('%d\t%d\t%f' % 
+                (length, 
+                count,
+                count/totalMapped), 
+                file=out)
+    out.close()
+
+if __name__ == '__main__':
+    main()
